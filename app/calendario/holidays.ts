3cbd9620e5490170a603123cf7fd86f147c2b020
Mário Ponte <ponteolavo30@gmail.com>
2025-04-01 22:50:09 +0100

    adding holidays object

---
+++ app/calendario/holidays.ts
@@ -0,0 +1,10 @@
+export const holidays = {
+    1: { 1: "Ano Novo" },
+    4: { 25: "Dia da Liberdade" },
+    5: { 1: "Dia do Trabalhador" },
+    6: { 10: "Dia de Portugal" },
+    8: { 15: "Dia da Assunção de Nossa Senhora" },
+    10: { 5: "Dia da Implantação da República" },
+    11: { 1: "Dia de todos os Santos" },
+    12: [{ 1: "Dia da Restauração da Independência" }, { 8: "Dia da Imaculada Conceição" }, { 25: "Natal" }]
+};
